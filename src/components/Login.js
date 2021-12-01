c52519a066507c5b285862f72bfebf4bede6fcc7
dtr200 <negevski@yandex.ru>
2021-12-02 01:20:02 +0300

    login switched from classcomponent to function one

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom';
 import ListErrors from './ListErrors';
-import React from 'react';
+import React, { useEffect } from 'react';
 import agent from '../agent';
 import { connect } from 'react-redux';
 import {
@@ -8,6 +8,8 @@ import {
   LOGIN,
   LOGIN_PAGE_UNLOADED
 } from '../constants/actionTypes';
+
+import styled from 'styled-components';
 
 const mapStateToProps = state => ({ ...state.auth });
 
@@ -22,76 +24,76 @@ const mapDispatchToProps = dispatch => ({
     dispatch({ type: LOGIN_PAGE_UNLOADED })
 });
 
-class Login extends React.Component {
-  constructor() {
-    super();
-    this.changeEmail = ev => this.props.onChangeEmail(ev.target.value);
-    this.changePassword = ev => this.props.onChangePassword(ev.target.value);
-    this.submitForm = (email, password) => ev => {
-      ev.preventDefault();
-      this.props.onSubmit(email, password);
-    };
-  }
+const Login = (props) => {
+  console.log('PROPS', props)
 
-  componentWillUnmount() {
-    this.props.onUnload();
-  }
 
-  render() {
-    const email = this.props.email;
-    const password = this.props.password;
-    return (
-      <div className="auth-page">
-        <div className="container page">
-          <div className="row">
+  const changeEmail = ev => props.onChangeEmail(ev.target.value);
+  const changePassword = ev => props.onChangePassword(ev.target.value);
+  const submitForm = (email, password) => ev => {
+    ev.preventDefault();
+    props.onSubmit(email, password);
+  };
 
-            <div className="col-md-6 offset-md-3 col-xs-12">
-              <h1 className="text-xs-center">Sign In</h1>
-              <p className="text-xs-center">
-                <Link to="/register">
-                  Need an account?
-                </Link>
-              </p>
+  useEffect(() => {
+    return () => {
+      props.onUnload();
+    }
+  }, []);
 
-              <ListErrors errors={this.props.errors} />
+  const email = props.email;
+  const password = props.password;
 
-              <form onSubmit={this.submitForm(email, password)}>
-                <fieldset>
+  return (
+    <div className="auth-page">
+      <div className="container page">
+        <div className="row">
 
-                  <fieldset className="form-group">
-                    <input
-                      className="form-control form-control-lg"
-                      type="email"
-                      placeholder="Email"
-                      value={email}
-                      onChange={this.changeEmail} />
-                  </fieldset>
+          <div className="col-md-6 offset-md-3 col-xs-12">
+            <h1 className="text-xs-center">Sign In</h1>
+            <p className="text-xs-center">
+              <Link to="/register">
+                Need an account?
+              </Link>
+            </p>
 
-                  <fieldset className="form-group">
-                    <input
-                      className="form-control form-control-lg"
-                      type="password"
-                      placeholder="Password"
-                      value={password}
-                      onChange={this.changePassword} />
-                  </fieldset>
+            <ListErrors errors={props.errors} />
 
-                  <button
-                    className="btn btn-lg btn-primary pull-xs-right"
-                    type="submit"
-                    disabled={this.props.inProgress}>
-                    Sign in
-                  </button>
+            <form onSubmit={submitForm(email, password)}>
+              <fieldset>
 
+                <fieldset className="form-group">
+                  <input
+                    className="form-control form-control-lg"
+                    type="email"
+                    placeholder="Email"
+                    value={email}
+                    onChange={changeEmail} />
                 </fieldset>
-              </form>
-            </div>
 
+                <fieldset className="form-group">
+                  <input
+                    className="form-control form-control-lg"
+                    type="password"
+                    placeholder="Password"
+                    value={password}
+                    onChange={changePassword} />
+                </fieldset>
+                <button
+                  className="btn btn-lg btn-primary pull-xs-right"
+                  type="submit"
+                  disabled={props.inProgress}>
+                  Sign in
+                </button>
+
+              </fieldset>
+            </form>
           </div>
+
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Login);
